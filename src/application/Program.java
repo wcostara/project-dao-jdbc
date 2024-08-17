d7771fa269f91ee8eeb82ff26780b9bc47919dee
Will Costa <wcostarabr@hotmail.com>
2024-08-17 20:27:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
 import model_entities.Departament;
+import model_entities.Seller;
+
+import java.util.Date;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
-        Departament obg = new Departament(1, "Books");
-        System.out.println(obg);
+        Departament obj = new Departament(1, "Books");
+        Seller sell = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(sell);
     }
 }
