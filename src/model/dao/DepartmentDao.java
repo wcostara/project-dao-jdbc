4d2444d6604169054fdee4fe4ac2fe0a41272b93
Will Costa <wcostarabr@hotmail.com>
2024-08-19 14:47:13 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model_entities.Departament;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert (Departament obj);
+    void update (Departament obj);
+    void deleteById (Integer id);
+    Departament findById (Integer id);
+    List<Departament> findAll();
+}
