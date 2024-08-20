8a6487e140e0aee54d72cbd97758534c0f67f61c
Will Costa <wcostarabr@hotmail.com>
2024-08-20 17:45:01 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,39 +1,11 @@
 package application;
 
-import model_dao.DaoFactory;
-import model_dao.SellerDao;
-import model_entities.Department;
-import model_entities.Seller;
-
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        System.out.println("=== TEST 1: seller findById =====");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
-
-        System.out.println("\n=== TEST 2: seller findByDepartment =====");
-        Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller obj : list) {
-            System.out.println(obj);
-        }
-
-        System.out.println("\n=== TEST 3: seller findAll =====");
-        list = sellerDao.findAll();
-        for (Seller obj : list) {
-            System.out.println(obj);
-        }
-
-        System.out.println("\n=== TEST 4: seller insert =====");
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("teste");
+    }
 }
